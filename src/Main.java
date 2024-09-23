20a1de190bdb6037146cad95475c0d6b00e68efb
楠z✰HisakoR <84510571+HisakoR@users.noreply.github.com>
2024-09-23 20:10:07 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,66 +9,59 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    //环境数据
-    private static final double FRICTION = 0.86;//摩擦力，控制球拍的速度和惯性
-    private static final double ACCELERATION = 0.86;//加速度
-    private static final double RADIUS = 30;//球拍大小
+    //Environment settings
+    private static final double FRICTION = 0.86;//greater = smoother
+    private static final double ACCELERATION = 0.86;//acceleration of bats
+    private static final double RADIUS = 30;//the size of bats
 
-    //A操作数据
+    //moving data of player1
     private double AvelocityX = 0;
     private double AvelocityY = 0;
     private boolean AisMovingLeft = false;
     private boolean AisMovingRight = false;
     private boolean AisMovingUp = false;
     private boolean AisMovingDown = false;
-    //A操作数据
 
-    //B操作数据
+    //moving data of player2
     private double BvelocityX = 0;
     private double BvelocityY = 0;
     private boolean BisMovingLeft = false;
     private boolean BisMovingRight = false;
     private boolean BisMovingUp = false;
     private boolean BisMovingDown = false;
-    //B操作数据
 
-    //冰球操作数据
+    //moving data of hockey
     private double HveloX = 0;
     private double HveloY = 0;
-    //冰球操作数据
 
-    //计分板
+    //scoreboard
     private int playerAsc = 0;
     private int playerBsc = 0;
     private boolean getScore = true;
 
     @Override
     public void start(Stage primaryStage) {
-        //初始化程序
-        Pane root = new Pane();//新操作面板
-        Scene scene = new Scene(root, 500, 800);//大小
-        Image backgroundImage = new Image("background.png");//背景
+        //initializing
+        Pane root = new Pane();//dashboard
+        Scene scene = new Scene(root, 500, 800);//size
+        Image backgroundImage = new Image("background.png");//background img
         BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
         Background bg = new Background(background);
-        root.setBackground(bg);//设置背景
-        primaryStage.setResizable(false);//不可调节大小
+        root.setBackground(bg);//set
+        primaryStage.setResizable(false);//cannot change size
 
-        //创建玩家A
+        //create players and hockey
         Circle playerA = new Circle(RADIUS, Color.BLUE);
-        //创建玩家B
         Circle playerB = new Circle(RADIUS, Color.RED);
-        //创建冰球
         Circle hockey = new Circle(20, Color.SILVER);
-        //玩家A的初始坐标，在桌台下半部分
+        //initialize the position
         playerA.setCenterX(250);
         playerA.setCenterY(600);
-        //玩家B的初始坐标，在桌台上半部分
         playerB.setCenterX(250);
         playerB.setCenterY(200);
-        //冰球的初始坐标
         hockey.setCenterX(250);
         hockey.setCenterY(395);
-        //对于玩家的移动判定
+        //move mechanism
         scene.setOnKeyPressed(event -> {
             KeyCode code = event.getCode();
 
@@ -85,7 +78,7 @@ public class Main extends Application {
                 AisMovingDown = true;
             }
 
-            if (code == KeyCode.U) {//重置冰球按钮
+            if (code == KeyCode.U) {//reset hockey
                 hockey.setCenterX(250);
                 hockey.setCenterY(395);
                 HveloY = 0;
@@ -136,16 +129,16 @@ public class Main extends Application {
             }
         });
 
-        //向场景添加并更新两个玩家
+        //render players
         root.getChildren().add(playerA);
         root.getChildren().add(playerB);
         root.getChildren().add(hockey);
 
-        //动画渲染
+        //render animations
         new AnimationTimer() {
             @Override
             public void handle(long now) {
-                //应用惯性系统
+                //acceleration
                 if (AisMovingLeft) {
                     AvelocityX -= ACCELERATION;
                 }
@@ -171,7 +164,7 @@ public class Main extends Application {
                     BvelocityY += ACCELERATION;
                 }
 
-                //应用摩擦系数
+                //add friction
                 AvelocityX *= FRICTION;
                 AvelocityY *= FRICTION;
                 BvelocityX *= FRICTION;
@@ -179,7 +172,7 @@ public class Main extends Application {
                 HveloX *= 0.95;
                 HveloY *= 0.95;
 
-                //检查球的运动轨迹
+                //check motion
                 double AnextX = playerA.getCenterX() + AvelocityX;
                 double AnextY = playerA.getCenterY() + AvelocityY;
 
@@ -189,7 +182,7 @@ public class Main extends Application {
                 double HnextX = hockey.getCenterX() + HveloX;
                 double HnextY = hockey.getCenterY() + HveloY;
 
-                //判断：如果球下一步会接触到屏幕边缘，将其反弹
+                //bound
                 if (AnextX < RADIUS || AnextX > scene.getWidth() - RADIUS) {
                     AvelocityX = - AvelocityX;
                 }
@@ -211,22 +204,20 @@ public class Main extends Application {
                     HveloY = - HveloY;
                 }
 
-                //A更新位置
+                //position update
                 playerA.setCenterX(playerA.getCenterX() + AvelocityX);
                 playerA.setCenterY(playerA.getCenterY() + AvelocityY);
-                //B更新位置
                 playerB.setCenterX(playerB.getCenterX() + BvelocityX);
                 playerB.setCenterY(playerB.getCenterY() + BvelocityY);
-                //冰球更新位置
                 hockey.setCenterX(hockey.getCenterX() + HveloX);
                 hockey.setCenterY(hockey.getCenterY() + HveloY);
 
-                //球拍与冰球的碰撞检测
+                //hit check
                 double distanceA = Math.sqrt(Math.pow(playerA.getCenterX() - hockey.getCenterX(), 2) + Math.pow(playerA.getCenterY() - hockey.getCenterY(), 2));
                 double distanceB = Math.sqrt(Math.pow(playerB.getCenterX()-hockey.getCenterX(), 2) + Math.pow(playerB.getCenterY()-hockey.getCenterY(), 2));
 
                 if (distanceA <= RADIUS + hockey.getRadius()) {
-                    //碰撞发生，根据碰撞角度调整冰球速度
+                    //change angle for a
                     double angle = Math.atan2(hockey.getCenterY() - playerA.getCenterY(), hockey.getCenterX() - playerA.getCenterX());
                     HveloX = AvelocityX * 3;
                     HveloY = AvelocityY * 3;
@@ -238,7 +229,7 @@ public class Main extends Application {
                 }
 
                 if (distanceB <= RADIUS + hockey.getRadius()) {
-                    //碰撞发生，根据碰撞角度调整冰球速度
+                    //change angle for b
                     double angle = Math.atan2(hockey.getCenterY() - playerB.getCenterY(), hockey.getCenterX() - playerB.getCenterX());
                     HveloX = BvelocityX * 3;
                     HveloY = BvelocityY * 3;
@@ -249,27 +240,26 @@ public class Main extends Application {
                     BvelocityY = -BvelocityY*2-1;
                 }
 
-                //得分判定
-                if (hockey.getCenterY() <= 0 && (hockey.getCenterX()>150 && hockey.getCenterX() < 350) && getScore) { // 球进入A得分区
+                //score
+                if (hockey.getCenterY() <= 0 && (hockey.getCenterX()>150 && hockey.getCenterX() < 350) && getScore) {
                     playerAsc++;
                     getScore = false;
-                } else if (hockey.getCenterY() >= 800 && (hockey.getCenterX()>150 && hockey.getCenterX() < 350) && getScore) { // 球进入B得分区
+                } else if (hockey.getCenterY() >= 800 && (hockey.getCenterX()>150 && hockey.getCenterX() < 350) && getScore) {
                     playerBsc++;
                     getScore = false;
                 }
 
-                // 更新窗口标题显示得分信息
+                //set output
                 primaryStage.setTitle("Simple Hockey! |P1| " + playerAsc + " VS " + playerBsc + " |P2|");
 
             }
         }.start();
 
-        //界面内容
+        //ui
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    //主要方法
     public static void main(String[] args) {
         launch(args);
     }
